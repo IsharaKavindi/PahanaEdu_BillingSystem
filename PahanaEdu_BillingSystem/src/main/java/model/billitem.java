8fb8222b75f2a403606cb87b40570b6a1c50cc6a
IsharaKavindi <isharajayasundara668@gmail.com>
2025-08-07 04:21:40 +0530

    billitem model

---
+++ PahanaEdu_BillingSystem/src/main/java/model/billitem.java
@@ -0,0 +1,43 @@
+package model;
+
+public class billitem {
+	
+	int billid;
+	String itemid;
+	String title;
+	int quantity;
+	int price;
+	
+	public int getBillid() {
+		return billid;
+	}
+	public void setBillid(int billid) {
+		this.billid = billid;
+	}
+	public String getItemid() {
+		return itemid;
+	}
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
