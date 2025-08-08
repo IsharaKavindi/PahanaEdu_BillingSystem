760a815bbfcd29d2ef054cbc27b98db0786ee37a
IsharaKavindi <isharajayasundara668@gmail.com>
2025-08-08 09:26:04 +0530

    customerBillServlet

---
+++ PahanaEdu_BillingSystem/src/main/java/servlet/customerBillServlet.java
@@ -0,0 +1,54 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.customer;
+import services.customerServices;
+
+
+@WebServlet("/customerBillServlet")
+public class customerBillServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public customerBillServlet() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		customer cus = new customer();
+		cus.setNic(request.getParameter("nic"));
+		cus.setName(request.getParameter("name"));
+		
+		customerServices service = new customerServices();
+		boolean status = service.validateCus(cus);
+		if(status) {
+		    customer getedCus = service.getCus(cus);
+
+		    RequestDispatcher dispatcher = request.getRequestDispatcher("generateBill.jsp");
+		    request.setAttribute("customer",getedCus);
+		    dispatcher.forward(request,response);
+		}else {
+		    RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+		    dispatcher.forward(request,response);
+		}
+		
+		doGet(request, response);
+	
+	}
+
+}
