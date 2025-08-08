f83ab2bfd5fdee47b0311c14c63fbd355a5c61fe
IsharaKavindi <isharajayasundara668@gmail.com>
2025-08-08 09:23:36 +0530

    bill model

---
+++ PahanaEdu_BillingSystem/src/main/java/model/bill.java
@@ -0,0 +1,70 @@
+package model;
+
+public class bill {
+	int billid;
+	String billnum;
+	String date;
+	String time;
+	int totalitems;
+	int total;
+	int points;
+	int cusid;
+	String cusnic;
+	
+	
+	public int getBillid() {
+		return billid;
+	}
+	public void setBillid(int billid) {
+		this.billid = billid;
+	}
+	public String getBillnum() {
+		return billnum;
+	}
+	public void setBillnum(String billnum) {
+		this.billnum = billnum;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getTotalitems() {
+		return totalitems;
+	}
+	public void setTotalitems(int totalitems) {
+		this.totalitems = totalitems;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public int getCusid() {
+		return cusid;
+	}
+	public void setCusid(int cusid) {
+		this.cusid = cusid;
+	}
+	public String getCusnic() {
+		return cusnic;
+	}
+	public void setCusnic(String cusnic) {
+		this.cusnic = cusnic;
+	}
+
+}
