ad630806112aff257dbcfbf1a6337af4cd06daaf
IsharaKavindi <isharajayasundara668@gmail.com>
2025-08-13 00:40:21 +0530

    clear bill function

---
+++ PahanaEdu_BillingSystem/src/main/java/servlet/clearBillServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/clearBillServlet")
+public class clearBillServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); 
+        if (session != null) {
+            session.removeAttribute("billList"); 
+        }
+
+        response.sendRedirect("billServlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response); 
+    }
+}
