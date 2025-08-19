79ef93bc5ec069994e0c741d92e9237cb3c1c7b0
IsharaKavindi <isharajayasundara668@gmail.com>
2025-08-19 16:54:38 +0530

    Admin login

---
+++ PahanaEdu_BillingSystem/src/main/java/servlet/adminLoginServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/adminLoginServlet")
+public class adminLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public adminLoginServlet() {
+        super();
+
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		    
+		    String username = request.getParameter("username");
+		    String password = request.getParameter("password");
+
+
+		    if("admin123".equals(username) && "a123".equals(password)) {
+		        request.setAttribute("popupMessage", "Login Successfully");
+		        RequestDispatcher dispatcher = request.getRequestDispatcher("adminDash.jsp");
+		        dispatcher.forward(request, response);
+		    } else {
+		        request.setAttribute("popupMessage", "Incorrect username or password");
+		        RequestDispatcher dispatcher = request.getRequestDispatcher("adminLogin.jsp");
+		        dispatcher.forward(request, response);
+		    }
+		
+	}
+
+}
---
+++ PahanaEdu_BillingSystem/src/main/webapp/adminLogin.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%> 
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/NewCss.css">
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body style="background: linear-gradient(135deg, #f5f7fa, #c3cfe2)">
+	<div class="box">
+	
+		<div class="topic">Admin Login</div>
+		<form action="adminLoginServlet" method="post" >
+		  <div class="mb-3">
+		    <label for="exampleInputUsername" class="form-label" >Username</label>
+		    <input type="text" class="form-control" id="exampleInputUsername" name="username">
+		  </div>
+		  <div class="mb-3">
+		    <label for="exampleInputPassword1" class="form-label" >Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+		  </div>
+	
+		  <button type="submit" class="btn btn-primary">Login</button>
+		</form>
+		
+		<c:if test="${not empty popupMessage}">
+		    <script>
+		        alert("${popupMessage}");
+		    </script>
+		</c:if>
+	</div>
+		
+</body>
+</html>
